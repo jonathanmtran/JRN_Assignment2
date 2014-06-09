3969619fd39553d4f247abc814e662111d380192
jonathanmtran <jonathanmtran@about.me>
2014-06-08 22:45:46 -0700

    Updated orderline model - Renamed business.LineItem to business.OrderLine - Created create and update methods

---
+++ src/java/business/OrderLine.java
@@ -0,0 +1,63 @@
+package business;
+
+import java.io.Serializable;
+import java.text.NumberFormat;
+
+public class OrderLine implements Serializable
+{
+	private int orderlineId;
+	private int orderId;
+	private Item item;
+    private int quantity;
+	
+	public OrderLine() {}
+    
+	public OrderLine setOrderlineId(int id) {
+		this.orderlineId = id;
+		return this;
+	}
+	
+	public int getOrderlineId() {
+		return this.orderlineId;
+	}
+	
+	public OrderLine setOrderId(int id) {
+		this.orderId = id;
+		return this;
+	}
+	
+	public int getOrderId() { 
+		return this.orderId;
+	}
+	
+	public OrderLine setItem(Item i) {
+       item = i;
+	   return this;
+	}
+   
+	public Item getItem() {
+       return item;
+	}
+
+    public OrderLine setQuantity(int quantity)
+    {
+        this.quantity = quantity;
+		return this;
+    }
+    
+    public int getQuantity()
+    { 
+        return quantity; 
+    }
+    
+    public double getSubtotal() { 
+        double total = item.getPrice() * quantity;
+        return total;
+    }
+    
+    public String getTotalCurrencyFormat()
+    {
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        return currency.format(this.getSubtotal());
+    }
+}
---
+++ src/java/data/OrderLineDB.java
@@ -0,0 +1,63 @@
+package data;
+
+import java.sql.*;
+import business.OrderLine;
+
+public class OrderLineDB {
+	public static int insert(OrderLine orderLine) {
+		ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+		
+		String query = "INSERT INTO orderline (orderID, itemID, quantity, subtotal) VALUES(?, ?, ?, ?))";
+		
+		try {
+			ps = connection.prepareStatement(query);
+			ps.setInt(1, orderLine.getOrderId());
+			ps.setInt(2, orderLine.getItem().getItemID());
+			ps.setInt(3, orderLine.getQuantity());
+			ps.setDouble(4, orderLine.getSubtotal());
+			
+			return ps.executeUpdate();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			
+			return 0;
+		}
+		finally {
+			DBUtil.closePreparedStatement(ps);
+			pool.freeConnection(connection);
+		}
+	}
+
+	public static int update(OrderLine orderLine) {
+		ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+		
+		String query = "UPDATE orderline SET " + 
+			"orderID = ?, itemID = ?, quantity = ?, subtotal = ? " +
+			"WHERE orderlineID = ?";
+		
+		try {
+			ps = connection.prepareStatement(query);
+			ps.setInt(1, orderLine.getOrderId());
+			ps.setInt(2, orderLine.getItem().getItemID());
+			ps.setInt(3, orderLine.getQuantity());
+			ps.setDouble(4, orderLine.getSubtotal());
+			ps.setInt(5, orderLine.getOrderlineId());
+			
+			return ps.executeUpdate();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			
+			return 0;
+		}
+		finally {
+			DBUtil.closePreparedStatement(ps);
+			pool.freeConnection(connection);
+		}
+	}		
+}
